e750dc474f9155e3d327462a04e706df9a7efc2d
pip <jrphiri@gmail.com>
2021-06-01 11:27:45 +0200

    created Login component

---
+++ components/Auth.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (email) => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signIn({ email });
+      if (error) throw error;
+    } catch {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + Next.js</h1>
+        <div className="description">
+          Sign in via magic link with your email below
+        </div>
+        <div>
+          <input
+            className="inputField"
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin(email);
+            }}
+            className={"button block"}
+            disabled={loading}
+          >
+            {loading ? <span>Loading</span> : <span>Send magic link</span>}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
